7bb703ac9a88c74b1acf43018281e7f9dce1e4b2
Wisdom Ochei <wisdomofgod2003@gmail.com>
2023-11-09 11:46:58 +0100

    Done with Creators Page

--- src/components/creator/CreatorItem.jsx
+++ src/components/creator/CreatorItem.jsx
@@ -1,13 +1,50 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
-const CreatorItem = () => {
+const CreatorItem = ({ creatorItem }) => {
+  const positions = creatorItem?.positions?.map((position) => position?.name);
+  const games = creatorItem?.games?.map((game) => game?.name);
+
   return (
-    <CreatorItemWrapper>
+    <CreatorItemWrapper
+      className="card"
+      style={{
+        background: `linear-gradient(rgba(7, 5, 27, 0.9), rgba(0, 0, 0, 0.95)), url(${creatorItem?.image_background}) center/cover no-repeat`,
+      }}
+    >
+      <div className="card-top">
+        <img src={creatorItem?.image} />
+      </div>
+      <div className="card-button text-white">
+        <h4 className="card-title">{creatorItem?.name}</h4>
+        <ul className="card-list-group text-white">
+          <li className="list-group-item">
+            <span className="item-left">Games Count: </span>
+            <span className="item-right">{creatorItem?.games_count}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="item-left">Positions: </span>
+            <span className="item-right">
+              {positions.length > 0 ? positions.join(", ") : "N/A"}
+            </span>
+          </li>
+          <li className="list-group-item">
+            <span className="item-left">Games: </span>
+            <span className="item-right">
+              {games.length > 0 ? games.join(", ") : "N/A"}
+            </span>
+          </li>
+        </ul>
+      </div>
     </CreatorItemWrapper>
-  )
-}
+  );
+};
 
 export default CreatorItem;
+
+CreatorItem.propTypes = {
+  creatorItem: PropTypes.object,
+};
 
 const CreatorItemWrapper = styled.div`
   min-height: 360px;
@@ -15,11 +52,11 @@ const CreatorItemWrapper = styled.div`
   padding: 16px 32px 24px 32px;
   text-align: center;
 
-  .card-title{
+  .card-title {
     font-size: 24px;
   }
 
-  .card-top{
+  .card-top {
     height: 150px;
     width: 150px;
     margin-right: auto;
@@ -30,18 +67,18 @@ const CreatorItemWrapper = styled.div`
     border: 2px solid var(--clr-white);
     transition: var(--transition-default);
 
-    &:hover{
+    &:hover {
       transform: scale(1.1);
     }
   }
 
-  .card-bottom{
+  .card-bottom {
     margin-top: 48px;
   }
 
-  .list-group-item{
+  .list-group-item {
     margin-top: 8px;
-    .item-left{
+    .item-left {
       font-weight: 600;
     }
   }
--- src/components/creator/CreatorList.jsx
+++ src/components/creator/CreatorList.jsx
@@ -1,14 +1,25 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
+import CreatorItem from "./CreatorItem";
 
-const CreatorList = () => {
-    return (
-        <CreatorListWrapper>
-        </CreatorListWrapper>
-    )
-}
+const CreatorList = ({ creators }) => {
+  return (
+    <CreatorListWrapper>
+      <div className="card-list">
+        {creators?.map((item) => (
+          <CreatorItem key={item.id} creatorItem={item} />
+        ))}
+      </div>
+    </CreatorListWrapper>
+  );
+};
 
 export default CreatorList;
 
+CreatorList.propTypes = {
+  creators: PropTypes.array,
+};
+
 const CreatorListWrapper = styled.div`
-    margin-top: 140px;
+  margin-top: 140px;
 `;
